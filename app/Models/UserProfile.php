78b2046a948631620fafc0ca212b9c7175e9c900
Tobias Grasse <tg@glancr.de>
2023-08-30 08:33:57 +0200

    define extra_fields cast on UserProfile
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -4,19 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class UserProfile extends Model
 {
-	use HasFactory;
+    use HasFactory;
 
-        public function user(): BelongsTo
-	{
-		return $this->belongsTo(User::class);
-	}
+    public $casts = [
+      'extra_field' => 'array',
+    ];
 
-	public function skills(): BelongsToMany
-	{
-		return $this->belongsToMany(Skill::class, 'user_profile_skill');
-	}
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function skills(): BelongsToMany
+    {
+        return $this->belongsToMany(Skill::class, 'user_profile_skill');
+    }
 }
